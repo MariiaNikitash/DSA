23d8b2ffb199bfa326bd202ffab7445e7cf93bbe
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2025-07-09 12:50:32 -0700

    solved partition LL

--- CodePath_TIP102/linkedLists1/adv2.2.py
+++ CodePath_TIP102/linkedLists1/adv2.2.py
@@ -32,8 +32,177 @@ mario = Player("Mario", "Mushmellow")
 luigi = Player("Luigi", "Standard LG")
 peach = Player("Peach", "Bumble V")
 
-print_linked_list(arr_to_ll([mario, luigi, peach])) # Mario -> Luigi -> Peach 
+#print_linked_list(arr_to_ll([mario, luigi, peach])) # Mario -> Luigi -> Peach 
 
-print_linked_list(arr_to_ll([peach]))
+#print_linked_list(arr_to_ll([peach]))
+
+
+
+# Problem 2 
+class Node:
+    def __init__(self, value=None, next=None):
+        self.value = value
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
+
+# Function with a bug!
+def remove_by_value(head, val):
+    if not head:
+        return None
+    if head.value == val:
+        return head.next  
+
+    current = head
+    while current.next:
+        if current.next.value == val:
+            current.next = current.next.next  # fixed
+            return head  
+        current = current.next
+
+    return head
+
+# Problem 3: Partition List Around Value
+# partitions the linked list around val such that all nodes
+# with values less than val come before nodes with values greater than or equal to val.
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
+
+def partition(head, val):
+    less_head, greater_head = Node(0), Node(0)
+    less, greater = less_head, greater_head
+
+    cur = head
+    while cur:
+        if cur.value < val:
+            less.next = cur
+            less = less.next
+        else:
+            greater.next = cur
+            greater = greater.next
+        cur = cur.next
+    
+    greater.next = None  # Important for cycles
+    less.next = greater_head.next
+    return less_head.next
+
+# Problem 4: Middle Match
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
+
+def middle_match(head, val):
+    if not head:
+        return False
+    slow, fast = head, head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    
+    return slow.value == val
+
+
+kart_choices = Node("Bullet Bike", Node("Wild Wing", Node("Pirahna Prowler")))
+tournament_tracks = Node("Rainbow Road", Node("Bowser Castle", Node("Sherbet Land", Node("Yoshi Valley"))))
+
+#print(middle_match(kart_choices, "Wild Wing")) # T
+#print(middle_match(tournament_tracks, "Bowser Castle")) # F
+
+# reverse List
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
+
+
+def reverse(head):
+    cur = head
+    prev = None
+    while cur:
+        next_node = cur.next
+        cur.next = prev
+        prev = cur
+        cur = next_node
+    return prev
+
+kart_choices = Node("Bullet Bike", Node("Wild Wing", Node("Pirahna Prowler")))
+
+#print_linked_list(reverse(kart_choices))
+# ==================================
+# Is summentrical Linked List
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
+
+def is_symmetric(head):
+    if not head or head.next is None:
+        return True
+    slow = fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    # reverse 2nd half
+    prev = None
+    while slow:
+        next_node = slow.next
+        slow.next = prev
+        prev = slow
+        slow = next_node
+    # compare 2 parts
+    left, right = head, prev
+    while right:
+        if left.value != right.value:
+            return False
+        left = left.next
+        right = right.next
+
+    return True
+
+
+
+head1 = Node("Bitterling", Node("Crawfish", Node("Bitterling")))
+head2 = Node("Bitterling", Node("Carp", Node("Koi")))
+
+print(is_symmetric(head1)) # T
+print(is_symmetric(head2)) # F
+
 
 
---
+++ CodePath_TIP102/linkedLists2/adv1.py
@@ -0,0 +1,165 @@
+# Linked Lst Cycle 2 LC Medium
+class Solution:
+    def detectCycle(self, head):
+        if not head:
+            return None
+        slow = fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        else:
+            return None
+
+        fast = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        return slow
+# ===============================================
+# Problem 1
+class Node:
+   def __init__(self, value, next=None):
+       self.value = value
+       self.next = next
+
+# For testing
+#def print_linked_list(head):
+#    current = head
+#    while current:
+#       print(current.value, end=" -> " if current.next else "\n")
+#        current = current.next
+
+def edit_dna_sequence(dna_strand, m, n):
+    current = dna_strand
+    
+    while current:
+        # Retain the first m nodes
+        for i in range(1, m):
+            if current is None:
+                return dna_strand
+            current = current.next
+
+        if current is None:
+            return dna_strand
+
+        # Now current is at the m-th node
+        # We will delete the next n nodes
+        temp = current.next
+        for j in range(n):
+            if temp is None:
+                break
+            temp = temp.next
+
+        # Connect the m-th node to the node after the n deleted nodes
+        current.next = temp
+
+        # Move current to the next retained node
+        current = temp
+
+    return dna_strand
+
+#dna_strand = Node(1, Node(2, Node(3, Node(4, Node(5, Node(6, Node(7, Node(8, Node(9, Node(10, Node(11, Node(12, Node(13)))))))))))))
+
+#print_linked_list(edit_dna_sequence(dna_strand, 2, 3))
+
+# 1 -> 2 -> 6 -> 7 -> 11 -> 12
+# Explanation: Keep the first (m = 2) nodes starting from the head of the linked List  
+# (1 -> 2) show in black nodes.
+# Delete the next (n = 3) nodes (3 -> 4 -> 5) show in red nodes.
+# Continue with the same procedure until reaching the tail of the Linked List.
+
+# ===========================================================
+# Problem 2: Protein Folding Loop Detection
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+def cycle_length(protein):
+    if not protein:
+        return []
+    slow,fast = protein, protein
+    res = []
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            break
+    else:
+        return []
+    s = slow
+    while True:
+        res.append(s.value)
+        s = s.next
+        if s == fast:
+            break
+
+    return res
+
+protein_head = Node('Ala', Node('Gly', Node('Leu', Node('Val'))))
+protein_head.next.next.next.next = protein_head.next 
+
+#print(cycle_length(protein_head)) # ['Gly', 'Leu', 'Val']
+
+# ===========================================================
+
+# Problem 3: Segmenting Protein Chains for Analysis
+#725. Split Linked List in Parts
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    if not head:
+        print("Empty List")
+        return
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
+
+def split_protein_chain(protein, k):
+    cur = protein
+    length = 0
+    # get length to later split into k segments
+    while cur:
+        length+=1
+        cur = cur.next
+    # find size of each seg
+    base_size = length // k 
+    extra = length % k # segs with 1 node leftover
+    cur = protein
+    res = []
+    for i in range(k):
+        head = cur
+        seg_size = base_size + (1 if i < extra else 0)
+
+        for j in range(seg_size - 1):
+            if cur:
+                cur = cur.next
+            
+        if cur:
+            next_seg = cur.next
+            cur.next = None
+            cur = next_seg
+        
+        res.append(head)
+
+
+    return res
+
+protein1 = Node('Ala', Node('Gly', Node('Leu', Node('Val', Node('Pro', Node('Ser', Node('Thr', Node('Cys'))))))))
+protein2 = Node('Ala', Node('Gly', Node('Leu', Node('Val'))))
+
+parts = split_protein_chain(protein1, 3)
+for part in parts:
+    print_linked_list(part)
+
+parts = split_protein_chain(protein2, 5)
+for part in parts:
+    print_linked_list(part)
