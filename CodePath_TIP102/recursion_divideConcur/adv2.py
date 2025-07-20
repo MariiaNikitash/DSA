e2119bec10ca16dd33581ccc219c131f4bb335d6
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2025-07-19 23:21:29 -0700

    matrix 2d bin search completed pseudo

--- CodePath_TIP102/recursion_divideConcur/adv2.py
+++ CodePath_TIP102/recursion_divideConcur/adv2.py
@@ -11,8 +11,8 @@ def check_stock(inventory, part_id):
     return False
 
 
-print(check_stock([1, 2, 5, 12, 20], 20))
-print(check_stock([1, 2, 5, 12, 20], 100))
+#print(check_stock([1, 2, 5, 12, 20], 20))
+#print(check_stock([1, 2, 5, 12, 20], 100))
 
 
 
@@ -24,15 +24,33 @@ def find_frequency_positions(transmissions, target_code):
     '''
 
 
-print(find_frequency_positions([5,7,7,8,8,10], 8))
-print(find_frequency_positions([5,7,7,8,8,10], 6))
-print(find_frequency_positions([], 0))
+#print(find_frequency_positions([5,7,7,8,8,10], 8))
+#print(find_frequency_positions([5,7,7,8,8,10], 6))
+#print(find_frequency_positions([], 0))
 
 '''
 (3, 4)
 (-1, -1)
 (-1, -1)
 '''
+
+def next_greatest_letter(letters, target):
+    l,r = 0, len(letters) -1
+    while l <= r:
+        mid = (l+r) // 2
+
+        if letters[mid] <= target:
+            l = mid + 1
+        else:   
+            res = letters[mid]
+            r = mid - 1
+    return letters[res]
+
+letters = ['a', 'a', 'b', 'c', 'c', 'c', 'e', 'h', 'w']
+
+print(next_greatest_letter(letters, 'a'))
+print(next_greatest_letter(letters, 'd'))
+print(next_greatest_letter(letters, 'y'))
 
 def find_closest_planets(planets, target_distance, k):
     pass
