22fbeba05d1e770be945f156781ada6001886be0
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2025-07-23 23:37:35 -0700

    trees started and completed U8

--- CodePath_TIP102/trees/adv1.py
+++ CodePath_TIP102/trees/adv1.py
@@ -20,8 +20,20 @@ def right_vine(root):
 def right_vine(root):
     if  not root:
         return []
+    return [root.val] + right_vine(root.right)
+
+
+# or with a helper
+def right_vine(root):
     res = []
-    return right_vine()
+    def helper(node):
+        if not node:
+            return []
+        res.append(node.val)
+        helper(node.right)
+    
+    helper(root)
+    return res
 
 
 class TreeNode:
@@ -101,7 +113,7 @@ inventory = TreeNode(40,
                     TreeNode(5, TreeNode(20)),
                             TreeNode(10, TreeNode(1), TreeNode(30)))
 
-print(sum_inventory(inventory))
+#print(sum_inventory(inventory))
 
 
 # -------------------------------------------------------
@@ -133,9 +145,9 @@ def helper(node):
 """
       +
      / \ 
-    /   \
+    /   /
    -     *
-  / \   / \
+  / \   / /
  4   2 10  2
 
  22
@@ -152,4 +164,116 @@ root.left.right = TreeNode(2)
 root.right.left = TreeNode(10)
 root.right.right = TreeNode(2)
 
-print(helper(root))
+#print(helper(root))
+
+
+# Add all the leaf nodes to array
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.val = value
+        self.left = left
+        self.right = right
+
+def get_most_specific(root):
+    if not root:
+        return []
+    if not root.left and not root.right:
+        return [root.val]
+    
+    l = get_most_specific(root.left)
+    r = get_most_specific(root.right)
+
+    return l + r
+
+
+"""
+           Plantae
+          /       /
+         /         /
+        /           \ 
+Non-flowering     Flowering
+   /      \       /        /
+Mosses   Ferns Gymnosperms Angiosperms
+                             /     /
+                        Monocots  Dicots
+"""
+plant_taxonomy = TreeNode("Plantae", 
+                          TreeNode("Non-flowering", TreeNode("Mosses"), TreeNode("Ferns")),
+                                  TreeNode("Flowering", TreeNode("Gymnosperms"), 
+                                          TreeNode("Angiosperms", TreeNode("Monocots"), TreeNode("Dicots"))))
+
+#print(get_most_specific(plant_taxonomy))
+
+# =================
+#Problem 7
+# count trees greater threshold
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.val = value
+        self.left = left
+        self.right = right
+
+def count_old_growth(root, threshold):
+    if not root:
+        return 0
+    count = 1 if root.val > threshold else 0
+    count += count_old_growth(root.left, threshold)
+    count += count_old_growth(root.right, threshold)
+
+    return count
+
+
+"""
+     100
+     /  /
+    /    /
+  1200  1500
+  /     /  /
+20    700  2600
+"""
+
+forest = TreeNode(100, 
+                  TreeNode(1200, TreeNode(20)),
+                          TreeNode(1500, TreeNode(700), TreeNode(2600)))
+
+print(count_old_growth(forest, 1000))
+
+
+
+# Same Treees 
+
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.val = value
+        self.left = left
+        self.right = right
+
+def is_identical(root1, root2):
+    if not root1 and not root2:
+        return True
+    if root1 and not root2 or root2 and not root1:
+        return False
+    if root1.val != root2.val:
+        return False
+    
+    return is_identical(root1.left, root2.left) and is_identical(root1.right, root2.right)
+
+"""
+      1                1
+     / \              / \
+    2   3            2   3  
+"""
+root1 = TreeNode(1, TreeNode(2), TreeNode(3))
+root2 = TreeNode(1, TreeNode(2), TreeNode(3))
+
+"""
+      1                1
+     /                  \
+    2                    2  
+"""
+
+root3 = TreeNode(1, TreeNode(2))
+root4 = TreeNode(1, None, TreeNode(2))
+
+print(is_identical(root1, root2))
+print(is_identical(root3, root4))
