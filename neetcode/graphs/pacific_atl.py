2a50d77cbaf39f3615fde77cdf56a188c6529a0b
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2025-08-17 23:15:37 -0700

    working on pacific Atlantic problem

---
+++ neetcode/graphs/pacific_atl.py
@@ -0,0 +1,15 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        ROWS, COLS = len(heights), len(heights[0])
+        pac, atl = set(), set()
+
+        def dfs(r, c, visit, prevHeight):
+            if ((r, c) in visit or
+                r < 0 or c < 0 or r == ROWS or c == COLS or
+                heights[r][c] < prevHeight):
+                return
+            visit.add((r, c))
+            dfs(r + 1, c, visit, heights[r][c])
+            dfs(r - 1, c, visit, heights[r][c])
+            dfs(r, c + 1, visit, heights[r][c])
+            dfs(r, c - 1, visit, heights[r][c])
