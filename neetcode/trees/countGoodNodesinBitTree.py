da4995056ef9657ea34218ef557a55640523fab1
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2025-09-07 23:26:30 -0700

    problem added

---
+++ neetcode/trees/countGoodNodesinBitTree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+'''
+Given: Binary Tree!!
+Out: number of nodes that are the biggest in the path from the root 
+'''
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        def dfs(node, maxVal):
+            if not node:
+                return 0
+
+            res = 1 if node.val >= maxVal else 0
+            maxVal = max(maxVal, node.val)
+            res += dfs(node.left, maxVal)
+            res += dfs(node.right, maxVal)
+            return res
+
+        return dfs(root, root.val)
